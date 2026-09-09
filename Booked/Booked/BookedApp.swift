b6e9caab8f38854b12dca5986a468536ca25db2d
Jiwon Jeong <jiwonjeong414@gmail.com>
2026-09-09 17:24:06 -0400

    Initial Commit

---
+++ Booked/Booked/BookedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookedApp.swift
+//  Booked
+//
+//  Created by jiwon jeong on 9/9/26.
+//
+
+import SwiftUI
+
+@main
+struct BookedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Booked/BookedTests/BookedTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookedTests.swift
+//  BookedTests
+//
+//  Created by jiwon jeong on 9/9/26.
+//
+
+import Testing
+@testable import Booked
+
+struct BookedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
